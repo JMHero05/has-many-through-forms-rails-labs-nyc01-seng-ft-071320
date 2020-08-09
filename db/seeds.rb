b5833a3c887d81f947720620072039b6bbfa0ad7
JMHero05 <jake.mills.05@gmail.com>
2020-08-08 22:07:22 -0400

    Lab Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+Post.destroy_all
+Category.destroy_all
+User.destroy_all
+Comment.destroy_all
+PostCategory.destroy_all
+
+20.times do
+  Category.create(name: Faker::Book.genre)
+end
+
+20.times do
+  Post.create(title: Faker::Book.title, content: Faker::Lorem.paragraph)
+end
+
+20.times do
+  User.create(username: Faker::Internet.username, email: Faker::Internet.email)
+end
+
+40.times do
+  Comment.create(content: Faker::Hipster.sentence, user_id: User.all.sample.id, post_id: Post.all.sample.id)
+end
+
+40.times do
+  PostCategory.create(post_id: Post.all.sample.id, category_id: Category.all.sample.id)
+end
